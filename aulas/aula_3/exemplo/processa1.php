d0f46405f63c73a54e2a7cda29ffa459fab934e6
byanca lourenço <159471845+byancalourenco@users.noreply.github.com>
2025-02-27 08:59:35 -0300

    Add files via upload

---
+++ aulas/aula_3/exemplo/processa1.php
@@ -0,0 +1,16 @@
+<?php
+
+    // página referente ao exemplo 1 - html
+
+    // o que vai receber 
+    $nome = $_REQUEST['cxnome'];     
+    $idade = $_REQUEST['cxidade'];
+
+    // POST E GET - precisa ser com letra maiuscula
+    // REQUEST - os dois :)
+
+    echo "nome preenchido: $nome <br>";
+    echo "idade preenchida: $idade <br>";
+
+
+?>
