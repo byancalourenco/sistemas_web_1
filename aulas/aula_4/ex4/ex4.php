8c16ea9b890e9aedf50194fad05bfe719184f5cc
byanca lourenço <159471845+byancalourenco@users.noreply.github.com>
2025-03-09 16:33:19 -0300

    Add files via upload

---
+++ aulas/aula_4/ex4/ex4.php
@@ -0,0 +1,19 @@
+<?php
+
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $data = $_POST['data'];
+    $cartao = $_POST['cartao'];
+
+    echo "DADOS INFORMADOS: <br>";
+    echo "nome: $nome <br>";
+    echo "email: $email <br>";
+    echo "data: $data <br>";
+    echo "bandeira do cartão: $cartao";
+    
+    // cartao 
+
+
+
+
+?>
